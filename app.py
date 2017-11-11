c2b11dfd116b8226f9ad241a078a252102298748
Phil Winder <phil@winderresearch.com>
2017-11-11 21:35:36 +0000

    Add status code.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import random
 import time
 
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, abort
 from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
 
 app = Flask(__name__)
 
 # A counter to count the total number of HTTP requests
-REQUESTS = Counter('http_request_total', 'Total HTTP Requests (count)', ['method', 'endpoint'])
+REQUESTS = Counter('http_request_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
 
 # A gauge (i.e. goes up and down) to monitor the total number of in progress requests
 IN_PROGRESS = Gauge('http_request_inprogress', 'Number of in progress HTTP requests')
@@ -23,17 +23,20 @@ TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seco
 # Helper annotation to increment a gauge when entering the method and decrementing when leaving.
 @IN_PROGRESS.track_inprogress()
 def hello_world():
-    REQUESTS.labels(method='GET', endpoint="/").inc()  # Increment the counter
+    REQUESTS.labels(method='GET', endpoint="/", status_code=200).inc()  # Increment the counter
     return 'Hello, World!'
 
-
+# Note I'm intentionally failing occasionally to simulate a flakey service.
 @app.route('/slow')
 @TIMINGS.time()
 @IN_PROGRESS.track_inprogress()
 def slow_request():
-    REQUESTS.labels(method='GET', endpoint="/slow").inc()
     v = random.expovariate(1.0 / 1.3)
+    if v > 3:
+        REQUESTS.labels(method='GET', endpoint="/slow", status_code=500).inc()
+        abort(500)
     time.sleep(v)
+    REQUESTS.labels(method='GET', endpoint="/slow", status_code=200).inc()
     return render_template_string('<h1>Wow, that took {{v}} s!</h1>', v=v)
 
 
@@ -41,7 +44,7 @@ def slow_request():
 @IN_PROGRESS.track_inprogress()
 @TIMINGS.time()
 def index(name):
-    REQUESTS.labels(method='GET', endpoint="/hello/<name>").inc()
+    REQUESTS.labels(method='GET', endpoint="/hello/<name>", status_code=200).inc()
     return render_template_string('<b>Hello {{name}}</b>!', name=name)
 
 
@@ -49,7 +52,7 @@ def index(name):
 @IN_PROGRESS.track_inprogress()
 @TIMINGS.time()
 def metrics():
-    REQUESTS.labels(method='GET', endpoint="/metrics").inc()
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
     return generate_latest(REGISTRY)
 
 
